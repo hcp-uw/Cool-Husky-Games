8c5c07947d00baf8d25807713b9375a23767672e
Aaditya Thukral <at45465@uw.edu>
2024-05-05 15:56:42 -0700

    Husky Colours

--- public/uploads/wordle/wordguesser.js
+++ public/uploads/wordle/wordguesser.js
@@ -18,6 +18,7 @@ async function init() {
   setLoading(isLoading);
 
   function addLetter(letter) {
+    console.log(letter);
     if (currGuess.length < ANSWER_LENGTH) {
       // Add letter to the end
       currGuess += letter;
